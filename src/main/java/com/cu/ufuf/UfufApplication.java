3d33b5f08adde4d31f285690d9f58a8e4c2e473f
EunbiK99 <07bee18@gmail.com>
2024-01-08 14:41:31 +0900

    first commit

---
+++ src/main/java/com/cu/ufuf/UfufApplication.java
@@ -0,0 +1,13 @@
+package com.cu.ufuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfufApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfufApplication.class, args);
+	}
+
+}
