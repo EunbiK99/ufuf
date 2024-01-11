ccebd3aab07a9509b894bc64e14fe84c21eb4780
jay-sim <pamiele@naver.com>
2024-01-12 06:18:07 +0900

    240112 진용
    1. 미팅프로필 인서트 로직 처리
    2. 미팅메인페이지 프로필 존재시 해당 로우 d-none처리
    3. 미팅검색, 생성 페이지만 추가

--- src/main/java/com/cu/ufuf/meeting/mapper/MeetingSqlMapper.java
+++ src/main/java/com/cu/ufuf/meeting/mapper/MeetingSqlMapper.java
@@ -2,9 +2,20 @@ package com.cu.ufuf.meeting.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.cu.ufuf.dto.MeetingProfileDto;
+import com.cu.ufuf.dto.MeetingSNSDto;
+
 @Mapper
 public interface MeetingSqlMapper {
 
     public int countProfilePk();
+
+    public int countSNSUrlByInputUrlValue(String value);
+
+    public void insertMeetingProfile(MeetingProfileDto meetingProfileDto);
+
+    public void insertNewSNS(MeetingSNSDto meetingSNSDto);
+
+    public int countMeetingProfileByUserId(int user_id);
 
 }
