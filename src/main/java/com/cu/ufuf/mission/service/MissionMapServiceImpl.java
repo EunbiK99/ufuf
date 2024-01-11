4323103cda9477493b7a007d6acad64bf9b12cf3
EunbiK99 <07bee18@gmail.com>
2024-01-11 18:01:51 +0900

    주문번호 인서트 작성중

--- src/main/java/com/cu/ufuf/mission/service/MissionMapServiceImpl.java
+++ src/main/java/com/cu/ufuf/mission/service/MissionMapServiceImpl.java
@@ -3,6 +3,9 @@ package com.cu.ufuf.mission.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+import java.text.SimpleDateFormat;
+
 import com.cu.ufuf.dto.MissionInfoDto;
 import com.cu.ufuf.merchan.mapper.MerchanSqlMapper;
 import com.cu.ufuf.mission.mapper.MissionMapSqlMapper;
@@ -14,6 +17,22 @@ public class MissionMapServiceImpl {
     private MissionMapSqlMapper missionMapsqlMapper;
     @Autowired
     private MerchanSqlMapper merchanSqlMapper;
+
+    public void insertOrder(){
+
+        String order_id = "MI1";
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		String today = sdf.format(new Date());
+
+        UUID uuid = UUID.randomUUID();
+        String ramdomUUID = uuid.toString().replace("-", "");
+
+        ramdomUUID = ramdomUUID.substring(0, 15);
+
+        order_id = order_id + today + ramdomUUID;
+
+    }
 
     public void registerMission(MissionInfoDto missionInfoDto){
 
