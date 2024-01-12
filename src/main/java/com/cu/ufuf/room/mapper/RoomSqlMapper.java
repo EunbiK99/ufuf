4f836bd077bca43de18ed096e3dcc6edcbbe2ffe
gaaawon <kawoc87@gmail.com>
2024-01-12 14:21:47 +0900

    방 상세페이지

--- src/main/java/com/cu/ufuf/room/mapper/RoomSqlMapper.java
+++ src/main/java/com/cu/ufuf/room/mapper/RoomSqlMapper.java
@@ -4,14 +4,17 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.cu.ufuf.dto.RoomGuestDto;
 import com.cu.ufuf.dto.RoomImageDto;
 import com.cu.ufuf.dto.RoomInfoDto;
 import com.cu.ufuf.dto.RoomOptionCategoryDto;
 import com.cu.ufuf.dto.RoomOptionDto;
+import com.cu.ufuf.dto.UserInfoDto;
 
 @Mapper
 public interface RoomSqlMapper {
 
+    //방 등록
     public int creatRoomInfoPk();
     public void roomInsert(RoomInfoDto roomInfoDto);
     public void insertRoomOption(RoomOptionDto roomOptionDto);
@@ -19,5 +22,21 @@ public interface RoomSqlMapper {
 
     public List<RoomOptionCategoryDto> selectRoomOptionCategoryAll();
 
+    //방 예약
+    public void insertRoomGuestInfo(RoomGuestDto roomGuestDto);
+
+    //방 리스트뽑게 
+     public List<RoomInfoDto> roomSelectAll();
+     public RoomInfoDto selectById(int room_info_id);
+
+     public UserInfoDto selectByUserId(int user_id);
+
+     //방 상세
+     public RoomInfoDto roomSelectById(int room_info_id);
+     public List<RoomImageDto> roomImageSelectById(int room_info_id);
+     public List<RoomOptionDto> roomOptionSelectById(int room_info_id);
+     public RoomOptionCategoryDto roomOptionCategorySelectById(int room_option_category_id);
+
+     public int roomImageCount(int room_info_id);
 
 }
